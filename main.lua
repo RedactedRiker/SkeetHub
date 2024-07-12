0fee893159971cea199a0616ab5086edc2147104
RedactedRiker <polecat324lapd@gmail.com>
2024-07-11 22:59:00 -0400

    update to lib

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- Snag the ui loader function thingy (loadstring the link, but dont call it)
-local uiLoader = loadstring(game:HttpGet('https://raw.githubusercontent.com/topitbopit/dollarware/main/library.lua'))
+local uiLoader = loadstring(game:HttpGet('https://github.com/RedactedRiker/SkeetHub/blob/main/library.lua'))
 -- Because of the way the library loads, settings are handled on the loadstring call
 local ui = uiLoader({
     rounding = false, -- Whether certain features get rounded 
